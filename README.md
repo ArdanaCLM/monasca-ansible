f5e3321e510254d03b61a5c1be14580f90ca91ea
Nicolas Bock <nicolas.bock@suse.com>
2017-09-22 10:26:26 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Monasca Ansible playbooks and roles for use with Ardana
+The Monasca team maintains public Ansible roles for use in installing Monasca. 
---
+++ ardana-ci/tests/validate-monasca-agent-down.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# (c) Copyright 2016 Hewlett Packard Enterprise Development LP
+# (c) Copyright 2017 SUSE LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+
+ansible-playbook -i hosts/verb_hosts monasca-agent-status.yml
+monasca_agent_status=$?
+echo "Monasca Agent Status is $monasca_agent_status"
+if [ $monasca_agent_status -eq 0 ]
+then
+  echo "Fail"
+  exit 1
+else
+  echo "OK"
+fi
